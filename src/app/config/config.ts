5f39fb0b1bc131ecd007d997455a4c96cab6033b
unknown <desarrollosoaint@gmail.com>
2021-08-17 09:02:50 -0400

    Config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,7 @@
 // tslint:disable-next-line: eofline
-export const BASE_URL = 'https://soniatback.herokuapp.com/api/v1';
-//export const Url_Back = 'http://localhost:8000/api/v1/';
-export const Url_Back = 'https://soaint-sa-chatbot.herokuapp.com/api/v1/';
+// export const BASE_URL = 'https://soniatback.herokuapp.com/api/v1/';
+export const BASE_URL = 'https://prueba-chatbot-sa.herokuapp.com/api/v1/';
+export const Url_Back = 'https://prueba-chatbot-sa.herokuapp.com/api/v1/';
+// export const BASE_URL = 'http://localhost:8000/api/v1/';
+// export const Url_Back = 'http://localhost:8000/api/v1/';
+
